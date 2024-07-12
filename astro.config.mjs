a6e2fef170cdea3bf3e5cf44123107fa5f6a9faa
empitsu <oneok0813@gmail.com>
2024-07-12 23:41:19 +0900

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import svelte from "@astrojs/svelte";
+import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://example.com",
-  integrations: [svelte()]
+  integrations: [svelte(), tailwind(), icon()]
 });
